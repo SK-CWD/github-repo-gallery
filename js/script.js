82d00052d50a6f3fd8fe3f71404338706cf9ebc2
SK-CWD <85139957+SK-CWD@users.noreply.github.com>
2021-08-12 16:24:32 -0500

    Added all repos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const overview = document.querySelector(".overview");//profile info will appear
 const username = "SK-CWD";//username on git hub
+const repoList = document.querySelector(".repo-list");//displays the list of repos
 
 //this async function will fetch info from my GitHub profile using API address
 const pulledFromGitHub = async function () {
-    const res = await fetch (`https://api.github.com/users/${username}`);
-    const data = await res.json();
+    const userInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
     console.log(data);
 
     displayInfo(data);
@@ -28,5 +29,26 @@ const displayInfo = function (data){
     </div>`;
 
     overview.append(div);
+    allRepos();
+};
+
+//async function to fetch all my repos
+const allRepos = async function () {
+    const res = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await res.json();
+    // console.log(repoData);
+
+    repoDescriptions(repoData);
+};
+
+//function will display information about each repo //Use repos as 
+//a parameter so that the function accepts the data returned from your last API call
+const repoDescriptions = function (repos) {
+    for(const repo of repos){
+        const eachRepo = document.createElement("li");
+        eachRepo.classList.add("repo");
+        eachRepo.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(eachRepo);
+    }
 };
 
